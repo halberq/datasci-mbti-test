e97e7c9a271af80f0ce86465e0769a7e100fe342
halberq <halbertmelquifallerofficial@gmail.com>
2026-08-02 21:02:53 +0800

    Rollback

--- client/script.js
+++ client/script.js
@@ -79,6 +79,7 @@ function submitAnswers() {
     .then(data => {
         finalType = data.type_code;
         resultMessage.textContent = `${username}, your results show that your MBTI is ${finalType}!`;
+        resultType.textContent = data.type_code;
         showPage(pageResult);
     })
     .catch(error => console.error("Error submitting answers:", error));
